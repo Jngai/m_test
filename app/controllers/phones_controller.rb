f194c4286cf989ddfcbd623666840e3dfa21b0f4
Judy Ngai <judy.ngai1297@gmail.com>
2020-07-06 08:38:45 -0400

    phone form

--- app/controllers/phones_controller.rb
+++ app/controllers/phones_controller.rb
@@ -1,4 +1,8 @@
 class PhonesController < ApplicationController
     def index 
     end
+
+    def check_number_validity
+        puts 'I got here'
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "home#index"
   resources :phones
+  match 'check_number_validity', to: 'phones#check_number_validity', via: [:post]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
