bcd88f49bab1a5dbe8363fc535cd322f8c63d19f
Judy Ngai <judy.ngai1297@gmail.com>
2020-07-06 09:04:35 -0400

    regex for phone number

--- app/controllers/phones_controller.rb
+++ app/controllers/phones_controller.rb
@@ -3,6 +3,10 @@ class PhonesController < ApplicationController
     end
 
     def check_number_validity
-        puts 'I got here'
+        if (/^(?:(?:\(?(?:00|\+)([1-4]\d\d|[1-9]\d?)\)?)?[\-\.\ \\\/]?)?((?:\(?\d{1,}\)?[\-\.\ \\\/]?){0,})(?:[\-\.\ \\\/]?(?:#|ext\.?|extension|x)[\-\.\ \\\/]?(\d+))?$/i.match(params["phone_number"]))
+          return head :ok
+        else
+          render :plain => "Must be a valid phone number", status: :unprocessable_entity
+        end
     end
 end
