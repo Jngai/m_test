b887863672d8495934ef859839c06b675a2633d9
Judy Ngai <judy.ngai1297@gmail.com>
2020-07-06 08:16:13 -0400

    db config

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_07_06_120609) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "phone_number"
