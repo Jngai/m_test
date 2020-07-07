88084aac88bf3dcb9ad7e1c72f45cbbd88dffbe4
Judy Ngai <judy.ngai1297@gmail.com>
2020-07-07 06:36:44 -0400

    success page

--- app/controllers/phones_controller.rb
+++ app/controllers/phones_controller.rb
@@ -4,7 +4,8 @@ class PhonesController < ApplicationController
 
     def check_number_validity
         if (/^(?:(?:\(?(?:00|\+)([1-4]\d\d|[1-9]\d?)\)?)?[\-\.\ \\\/]?)?((?:\(?\d{1,}\)?[\-\.\ \\\/]?){0,})(?:[\-\.\ \\\/]?(?:#|ext\.?|extension|x)[\-\.\ \\\/]?(\d+))?$/i.match(params["phone_number"]))
-          return head :ok
+          #render :plain => "Its a valid number", status: :ok
+          redirect_to success_page_path
         else
           render :plain => "Must be a valid phone number", status: :unprocessable_entity
         end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root to: "home#index"
   resources :phones
   match 'check_number_validity', to: 'phones#check_number_validity', via: [:post]
+  match 'success_page', to: 'phones#success_page', via: [:get]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
